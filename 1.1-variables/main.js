f33647892636e54e211e607df1642229d3cd0cb5
g0lo8a <c_76@bk.ru>
2020-03-20 14:13:52 +0300

    🔥

--- 1.1-variables/main.js
+++ 1.1-variables/main.js
@@ -1,8 +1,8 @@
-function calculateTask1(){
+function calculateTask1() {
     let algebra = Number.parseInt(document.getElementById("algebra").value);
     let geography = Number.parseInt(document.getElementById("geography").value);
     let physics = Number.parseInt(document.getElementById("physics").value);
-    if(!algebra || !geography || !physics){
+    if (!algebra || !geography || !physics) {
         alert("Введите валидные значения");
         return;
     }
@@ -11,18 +11,18 @@ function calculateTask1(){
     document.getElementById("task1Result").innerText = `Средняя оценка равна: ${taskResult}`;
 }
 
-function calculateTask2(){
+function calculateTask2() {
     let userName = document.getElementById("username").value;
-    if(!userName){
+    if (!userName) {
         alert("Введите имя пользователя");
         return;
     }
-    
+
     let taskResult = sayHello(userName);
     document.getElementById("task2Result").innerText = taskResult;
 }
 
-function calculateTask3(){
+function calculateTask3() {
     let taskResult = calculateFormula();
     document.getElementById("task3Result").innerText = taskResult;
 }
