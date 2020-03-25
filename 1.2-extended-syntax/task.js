9dd98bc59f038735ace906a6c3c9267937899066
g0lo8a <c_76@bk.ru>
2020-03-25 20:01:06 +0300

    🔥

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -4,13 +4,16 @@ function getResult(a, b, c) {
     let d = (b * b) - (4 * a * c),
         x = [];
     if (d < 0) {
+
         return x;
     } else if (d === 0) {
         x.push(-b / (2 * a));
+
         return x;
     } else if (d > 0) {
         x.push((-b + Math.sqrt(d)) / (2 * a));
         x.push((-b - Math.sqrt(d)) / (2 * a));
+
         return x;
     }
 }
@@ -21,8 +24,10 @@ function getAverageMark(marks) {
     } else if (marks.length > 5) {
         console.log('Введено больше пяти оценок');
         marks.splice(5);
+
         return marks.reduce((sum, current) => sum + current, 0) / marks.length;
     } else {
+
         return marks.reduce((sum, current) => sum + current, 0) / marks.length;
     }
 }
@@ -31,8 +36,10 @@ function askDrink(name, dateOfBirthday) {
     let date = new Date(),
         year = date.getFullYear() - dateOfBirthday.getFullYear();
     if (year > 18) {
+
         return `Не желаете ли олд-фэшн, ${name}?`;
     } else {
+
         return `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
     }
 }
