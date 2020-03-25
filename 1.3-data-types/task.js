0a7c17e3182dec86863aa150c90e7596a5431ef8
g0lo8a <c_76@bk.ru>
2020-03-25 08:58:06 +0300

    🔥

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,23 @@
+'use strict';
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    let loanBody = amount - contribution,
+        p = percent / 100 / 12,
+        nowDate = new Date(),
+        month = date.getMonth() - nowDate.getMonth() + (12 * (date.getFullYear() - nowDate.getFullYear())),
+        totalAmount = +(loanBody * (p + p / (((1 + p) ** month) - 1)) * month).toFixed(2);
+
+    console.log(totalAmount);
+
+    return totalAmount;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    if (!name) {
+
+        return 'Привет, мир! Меня зовут Аноним';
+    } else {
+
+        return `Привет, мир! Меня зовут ${name}`;
+    }
 }
