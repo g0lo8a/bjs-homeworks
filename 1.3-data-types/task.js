dddabeb9e9dc731493e8031d3fe2abf0d0e2364d
g0lo8a <c_76@bk.ru>
2020-03-29 11:42:54 +0300

    🔥

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,11 +1,10 @@
 'use strict';
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    if (isNaN(percent) || isNaN(contribution) || isNaN(amount)) {
+    if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || date <= (Date.now() + (60000 * 60 * 24 * 30))) {
 
         return 'Данные не верны!';
-    }
-    if (percent >= 10 && contribution >= 0 && amount && date > (Date.now() + (60000 * 60 * 24 * 30))) {
+    } else if (percent >= 10 && contribution >= 0 && amount >= 10000) {
         let loanBody = amount - contribution,
             p = percent / 100 / 12,
             nowDate = new Date(),
