85784067612377ec498539d4ea628a3be9720d2b
g0lo8a <c_76@bk.ru>
2020-03-29 14:15:43 +0300

    🔥

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -4,28 +4,25 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
     if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || date <= (Date.now() + (60000 * 60 * 24 * 30))) {
 
         return 'Данные не верны!';
-    } else if (percent >= 10 && contribution >= 0 && amount >= 10000) {
-        let loanBody = amount - contribution,
-            p = percent / 100 / 12,
-            nowDate = new Date(),
-            month = date.getMonth() - nowDate.getMonth() + (12 * (date.getFullYear() - nowDate.getFullYear())),
-            totalAmount = +(loanBody * (p + p / (((1 + p) ** month) - 1)) * month).toFixed(2);
+    }
 
-        console.log(totalAmount);
+    const isValidParams = percent >= 10 && contribution >= 0 && amount >= 10000;
 
-        return totalAmount;
-    } else {
-
+    if (!isValidParams) {
         return 'Данные не верны!';
     }
+    const loanBody = amount - contribution,
+        p = percent / 100 / 12,
+        nowDate = new Date(),
+        month = date.getMonth() - nowDate.getMonth() + (12 * (date.getFullYear() - nowDate.getFullYear())),
+        totalAmount = +(loanBody * (p + p / (((1 + p) ** month) - 1)) * month).toFixed(2);
+
+    console.log(totalAmount);
+
+    return totalAmount;
 }
 
 function getGreeting(name) {
-    if (!name) {
 
-        return 'Привет, мир! Меня зовут Аноним';
-    } else {
-
-        return `Привет, мир! Меня зовут ${name}`;
-    }
+    return `Привет, мир! Меня зовут ${name || 'Аноним'}`;
 }
